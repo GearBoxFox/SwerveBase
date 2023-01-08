b35f39a5a60e82f9a0c54b7c906e977c822af231
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2023-01-08 10:49:28 -0500

    created io interface for working with advantagekit

---
+++ SwerveBase/src/main/java/frc/robot/subsystems/SwerveIO.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public interface SwerveIO {
+    @AutoLog
+    public class SwerveIOInput{
+        public SwerveModuleState frState = new SwerveModuleState();
+        public SwerveModuleState flState = new SwerveModuleState();
+        public SwerveModuleState brState = new SwerveModuleState();
+        public SwerveModuleState blState = new SwerveModuleState();
+
+        public double gyroYaw = 0.0;
+    }
+
+    public default void updateInputs(SwerveIOInput inputs){
+        /**update the drivetrains inputs */
+    }
+
+    public default void setModuleState(double xTranslation, double yTranslation, double zRotation){
+        /**Update module states based on driver inpuy */
+    }
+}
