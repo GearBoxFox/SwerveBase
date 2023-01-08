5d84cc5dacbc697fff46d2f41a84e371f99a5c2a
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2023-01-08 10:40:54 -0500

    removed unused imports/iles from template

---
+++ SwerveBase/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SwerveBase";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "d208778bb146360aca18c510f8801a8e8a850c12";
+  public static final String GIT_DATE = "2023-01-08 10:39:41 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2023-01-08 10:39:46 EST";
+  public static final long BUILD_UNIX_TIME = 1673192386150L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
